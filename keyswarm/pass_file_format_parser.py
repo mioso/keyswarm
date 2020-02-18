402ad2050a479ebc35b8e8d8e811f492782184fc
Ole Just <oju@mioso.com>
2020-02-18 17:35:46 +0100

    clean and fix up PassFile

--- keyswarm/pass_file_format_parser.py
+++ keyswarm/pass_file_format_parser.py
@@ -1,5 +1,7 @@
 from re import search, compile
 from os import path
+import logging
+
 from .gpg_handler import decrypt
 
 
@@ -8,12 +10,16 @@ class PassFile(object):
     A representation of a pass file.
     """
     def __init__(self, root_path=None, name=None):
+        logger = logging.getLogger(__name__)
+        logger.debug('PassFile.__init__: %r %r)', root_path, name)
         try:
-            r = compile('^(.*)\.gpg$')
-            m = r.match(name)
-            self.name = m.group(1)
-        except:
+            regex = compile(r'^(.*)\.gpg$')
+            match = regex.match(name)
+            self.name = match.group(1)
+        except (IndexError, TypeError):
             self.name = name
+        logger.debug('PassFile.__init__: self.name: %r', self.name)
+        self.root_path = root_path
         gpg_file = path.join(root_path, name) if name and root_path else None
         self.file = gpg_file
         self.password = str()
@@ -22,13 +28,19 @@ class PassFile(object):
         if gpg_file:
             lines = decrypt(gpg_file).split('\n')
             self.password = lines.pop(0)
-            while search(r'^.*:.*$', lines[0]):
-                line = lines[0].split(':', maxsplit=1)
-                attribute_key = line[0]
-                attribute_value = line[1]
-                self.attributes.append((attribute_key, attribute_value))
-                lines.pop(0)
+            logger.debug('PassFile.__init__: lines: %r', lines)
+            try:
+                while search(r'^.*:.*$', lines[0]):
+                    line = lines[0].split(':', maxsplit=1)
+                    attribute_key = line[0]
+                    attribute_value = line[1]
+                    self.attributes.append((attribute_key, attribute_value))
+                    lines.pop(0)
+            except IndexError:
+                pass
+            logger.debug('PassFile.__init__: self.attributes: %r', self.attributes)
             self.comments = '\n'.join(lines)
+            logger.debug('PassFile.__init__: self.comments: %r', self.comments)
 
     def __repr__(self):
         return f'PassFile(root_path={repr(self.root_path)}, name={repr(self.name)})'
@@ -48,8 +60,7 @@ class PassFile(object):
             output = '{output}{comments}'.format(output=output, comments=self.comments)
         if output:
             return output
-        else:
-            raise ValueError
+        raise ValueError
 
     def __str__(self):
         return self.get_cleartext()
